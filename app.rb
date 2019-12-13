cba26a0b3e8101e7554568cf27a4c00035e1e995
lauMv <42174201+lauMv@users.noreply.github.com>
2019-12-13 06:37:20 -0400

    mover autos

--- app.rb
+++ app.rb
@@ -8,6 +8,8 @@ require './lib/auto'
   $ejeY
   $instrucciones
   $auto
+  $lista_autos
+  $cant_autos=0
 
   get '/' do
       @nombre = params{:nombre}
@@ -21,19 +23,23 @@ require './lib/auto'
 
   get '/verTodo' do
     @punto_cardinal = params[:punto_cardinal]
-    @ancho = params[:ancho]
-    @alto = params[:alto]
+    @ancho = params[:ancho].to_i
+    @alto = params[:alto].to_i
     @ejeX = params[:ejeX].to_i
     @ejeY = params[:ejeY].to_i
     @instrucciones = params[:instrucciones]
     @auto = Auto.new(@ejeX.to_i, @ejeY.to_i,@punto_cardinal.to_s,@instrucciones.to_s)
+    @lista_autos =[@auto]
     if (@ancho.to_i>-1 && @alto.to_i>-1 && @ejeX.to_i>-1 && @ejeY.to_i>-1 && @punto_cardinal == 'n' || @punto_cardinal == 's' || @punto_cardinal == 'e' || @punto_cardinal == 'o')
-      erb :mirar
-    else
-      erb :error
-    end
- end
+        erb :mirar
+      else
+        erb :error
+      end
+  end
 
 public def simularTodo
-  return @auto.simular(@auto.direcciones.to_s)
+
+  @lista_autos.each {|auto|
+    return auto.simular(auto.direcciones.to_s)}
+
 end
--- lib/Auto.rb
+++ lib/Auto.rb
@@ -1,64 +1,64 @@
 class Auto
 
-    public def initialize(x,y,punto, direcciones)
-      @ejeX = x
-      @ejeY = y
-      @punto_cardinal = punto
-      @direcciones = direcciones
-    end
-  
-    def direcciones
-      @direcciones
-    end
-  
-    def avanzar()
-      if  @punto_cardinal == 'n'
-          @ejeY+=1
-      elsif @punto_cardinal == 's'
-          @ejeY-=1
-      elsif @punto_cardinal == 'e'
-          @ejeX+=1
-      elsif @punto_cardinal == 'o'
-          @ejeX-=1
-      end
-    end
-  
-    def derecha()
-      if  @punto_cardinal == 'n'
-          @punto_cardinal = 'e'
-      elsif @punto_cardinal == 's'
-          @punto_cardinal = 'o'
-      elsif @punto_cardinal == 'e'
-          @punto_cardinal = 's'
-      elsif @punto_cardinal == 'o'
-          @punto_cardinal = 'n'
-      end
-    end
-  
-    def izquierda()
-      if  @punto_cardinal == 'n'
-          @punto_cardinal = 'o'
-      elsif @punto_cardinal == 's'
-          @punto_cardinal = 'e'
-      elsif @punto_cardinal == 'e'
-          @punto_cardinal = 'n'
-      elsif @punto_cardinal == 'o'
-          @punto_cardinal = 's'
-      end
-    end
-  
-    public def simular(direcciones)
-      @direcciones.to_s.each_char{ |c|
-          case c
-          when 'a'
-              avanzar()
-          when 'd'
-              derecha()
-          when 'i'
-              izquierda()
-          end
-      }
-      return @ejeX.to_i, @ejeY.to_i, @punto_cardinal.to_s
-  
+  public def initialize(x,y,punto, direcciones)
+    @ejeX = x
+    @ejeY = y
+    @punto_cardinal = punto
+    @direcciones = direcciones
+  end
+
+  def direcciones
+    @direcciones
+  end
+
+  def avanzar()
+    if  @punto_cardinal == 'n'
+        @ejeY+=1
+    elsif @punto_cardinal == 's'
+        @ejeY-=1
+    elsif @punto_cardinal == 'e'
+        @ejeX+=1
+    elsif @punto_cardinal == 'o'
+        @ejeX-=1
     end
   end
+
+  def derecha()
+    if  @punto_cardinal == 'n'
+        @punto_cardinal = 'e'
+    elsif @punto_cardinal == 's'
+        @punto_cardinal = 'o'
+    elsif @punto_cardinal == 'e'
+        @punto_cardinal = 's'
+    elsif @punto_cardinal == 'o'
+        @punto_cardinal = 'n'
+    end
+  end
+
+  def izquierda()
+    if  @punto_cardinal == 'n'
+        @punto_cardinal = 'o'
+    elsif @punto_cardinal == 's'
+        @punto_cardinal = 'e'
+    elsif @punto_cardinal == 'e'
+        @punto_cardinal = 'n'
+    elsif @punto_cardinal == 'o'
+        @punto_cardinal = 's'
+    end
+  end
+
+  public def simular(direcciones)
+    @direcciones.to_s.each_char{ |c|
+        case c
+        when 'a'
+            avanzar()
+        when 'd'
+            derecha()
+        when 'i'
+            izquierda()
+        end
+    }
+    return @ejeX.to_i, @ejeY.to_i, @punto_cardinal.to_s
+
+  end
+end
